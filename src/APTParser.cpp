b23db967b48bd2c4cc80c7a8770e8223f75f37b4
Bryce Cordill <brycecordill@gmail.com>
2019-08-03 16:47:14 -0700

    Fixed bug where apt pacakge names would be missing a letter

--- src/APTParser.cpp
+++ src/APTParser.cpp
@@ -6,24 +6,29 @@ void APTParser::parseAndWrite() {
         if (ln.find("following NEW") != std::string::npos) {
             break;
         }
+    }
 
-        while (GET_INPUT_LINE) {
-            if (fH->getInFile()->eof()) {
-                break;
-            }
-            // If the line isn't indented (no longer in package list)
-            else if (ln.front() != ' ') {
-                break;
-            }
-
-            // Remove the first space
-            ln.erase(ln.begin());
-            // Replace the last char (\n) with a space
-            ln.replace(ln.length() - 1, 1, " ");
-
-            // Write out the modified string
-            fH->getOutFile()->write(ln.c_str(), ln.length());
+    while (GET_INPUT_LINE) {
+        if (fH->getInFile()->eof()) {
+            break;
         }
+        // If the line isn't indented (no longer in package list)
+        else if (ln.front() != ' ') {
+            break;
+        }
+
+        // Remove the first space
+        ln.erase(ln.begin());
+        // Replace the last char (\n) with a space
+        unsigned long pos = ln.find_last_of('\n');
+        if (pos != std::string::npos) {
+            std::cout << pos << "\n";
+            std::cout << ln.length() << "\n";
+            ln.replace(pos, 1, " ");
+        }
+
+        // Write out the modified string
+        fH->getOutFile()->write(ln.c_str(), ln.length());
     }
 
     // Write out a newline at the end of the file
